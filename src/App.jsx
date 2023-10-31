8bcca6ffd5884f2fbaa58aba080c2d5377d67bb1
nansinee <nansinee.si@ku.th>
2023-10-31 23:58:23 +0700

    fetch-product

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { ToastContainer } from "react-toastify"
-import Route from "./routes/Route"
-import { useEffect } from "react"
+import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchDataUser } from "./stores/slices/authSlice";
 import { getAccessToken } from "./utils/local-storage";
+import { fetchAllProduct } from "./stores/slices/productSlice";
+import Route from "./routes/Route";
 
 function App() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    if(getAccessToken()) dispatch(fetchDataUser());
-  }, []);
+    useEffect(() => {
+        if (getAccessToken()) {
+            dispatch(fetchDataUser());
+            dispatch(fetchAllProduct());
+        }
+    }, []);
 
-  return (
-    <>
-      <Route /> 
-      <ToastContainer
-        position="bottom-center"
-        autoClose={3000}
-        theme="colored"
-      />
-    </>
-  )
+    return (
+        <>
+            <Route />
+            <ToastContainer position="bottom-center" autoClose={3000} theme="colored" />
+        </>
+    );
 }
 
-export default App
+export default App;
