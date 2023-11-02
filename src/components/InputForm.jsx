d54788b33dba79e113407f37f3f2d3c00e274971
Saint <p.leelayuthto@hotmail.com>
2023-11-02 12:29:17 +0700

    ui navbar category

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,6 +1,6 @@
-export default function InputForm({ type = "text", placeholder, onChange, value, styles, isError, name }) {
+export default function InputForm({ type = "text", placeholder, onChange, value, styles, isError, name, className }) {
     return (
-        <div className="w-full text-lg pt-4 font-light ">
+        <div className={`w-full text-lg pt-4 font-light ${className} `}>
             <input
                 name={name}
                 type={type}
@@ -12,8 +12,7 @@ export default function InputForm({ type = "text", placeholder, onChange, value,
                         ? "w-full rounded-full outline-none border-2 px-4 py-2  focus:border-1 border-error-light-soft focus:ring-2 focus:ring-error-light  "
                         : "w-full rounded-full outline-none border-2 px-4 py-2  focus:border-1 border-main focus:ring-2 focus:ring-main-dark"
                 }`}
-            >
-            </input>
+            ></input>
         </div>
     );
 }
