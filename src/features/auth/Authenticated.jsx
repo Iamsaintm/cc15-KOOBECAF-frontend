ed0507b012c3f48b12f3408cfb0e428c1c7cc9f0
Saint <p.leelayuthto@hotmail.com>
2023-11-06 12:56:43 +0700

    edit authenticated

--- src/features/auth/Authenticated.jsx
+++ src/features/auth/Authenticated.jsx
@@ -1,10 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { getAccessToken } from "../../utils/local-storage";
+import { useSelector } from "react-redux";
 
 function Authenticated({ children }) {
     const authUser = getAccessToken();
+    const user = useSelector((state) => state.auth.authUserData);
 
-    if (!authUser) {
+    if (!user && !authUser) {
         return <Navigate to="/login" />;
     }
 
