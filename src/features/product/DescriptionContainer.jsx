d66e23a2c399948b7bce971cedda179743979eda
tha3atchai <thawatchaionin@gmail.com>
2023-11-16 11:33:20 +0700

    fix: bug and add:skeleton

--- src/features/product/DescriptionContainer.jsx
+++ src/features/product/DescriptionContainer.jsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 function DescriptionContainer({ error }) {
     const dispatch = useDispatch();
     const { description } = useSelector((state) => state.product.inputProduct);
+    const { loading } = useSelector((state) => state.product);
 
     const onChangeInput = (e) => {
         const fieldName = e.target.name;
@@ -27,17 +28,18 @@ function DescriptionContainer({ error }) {
         <>
             <div className="flex flex-col gap-4">
                 <div className="text-xl font-bold">Description</div>
-                {skeleton ? (
-
-                <div className="h-24 bg-blue-200 rounded-md border-2">
-                    <textarea
-                        name="description"
-                        value={description}
-                        onChange={onChangeInput}
-                        className="resize-none w-full h-full rounded-md"
-                    />
-                </div>
-                ) : (  <Skeleton containerClassName="flex-1" height={80} /> )}
+                {skeleton && !loading ? (
+                    <div className="h-24 bg-blue-200 rounded-md border-2">
+                        <textarea
+                            name="description"
+                            value={description}
+                            onChange={onChangeInput}
+                            className="resize-none w-full h-full rounded-md"
+                        />
+                    </div>
+                ) : (
+                    <Skeleton containerClassName="flex-1" height={80} />
+                )}
                 {error.description && (
                     <InputErrorMessage message={"Must be less than or equal to 100 characters long"} />
                 )}
