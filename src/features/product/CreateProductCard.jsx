99b0719638c0cf12769286eea69efa7d3d8de128
pputthanon <p.putthanon@gmail.com>
2023-11-16 11:34:14 +0700

    skeleton on wishlist selling choose list type and edit img in choose list type

--- src/features/product/CreateProductCard.jsx
+++ src/features/product/CreateProductCard.jsx
@@ -18,15 +18,16 @@ function CreateProductCard({ src, header, content, to }) {
                 to={to}
                 className="flex flex-col justify-center items-center gap-2 bg-white shadow-md shadow-main rounded-lg"
             >
-                <div className="w-16 bg-blue-200 aspect-square rounded-full">
-                    <img
-                        src={skeleton ? src : <Skeleton width={80} height={80} />}
-                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    />
-                </div>
+                {skeleton ? (
+                    <div className="w-16 bg-blue-200 aspect-square rounded-full">
+                        <img src={src} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+                    </div>
+                ) : (
+                    <Skeleton width={64} height={64} circle={true} />
+                )}
                 <div className="mt-1 text-sm text-gray-500">{skeleton ? header : <Skeleton width={100} />}</div>
                 <div className="mt-1 text-sm text-gray-500">
-                    {skeleton ? content : <Skeleton width={130} count={3} />}
+                    {skeleton ? content : <Skeleton width={150} count={2} />}
                 </div>
             </Link>
         </>
