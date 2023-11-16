5364cd2255c341e21f6e6d2153ee856e91f42fb2
pputthanon <p.putthanon@gmail.com>
2023-11-16 11:51:30 +0700

    edit grid on Homepage and wishlistPage

--- src/features/product/ProductCard.jsx
+++ src/features/product/ProductCard.jsx
@@ -14,7 +14,7 @@ function ProductCard({ src = defaultProduct, productPrice = "1,050,000", product
 
     return (
         <>
-            <div className="w-44 h-[278px] bg-white rounded-md shadow-main shadow-md">
+            <div className="w-52 h-[300px] bg-white rounded-md shadow-main shadow-md">
                 <div className="w-full overflow-hidden bg-gray-200 group-hover:opacity-75 h-48 rounded-t-md">
                     {skeleton ? (
                         <img className="h-full w-full object-cover object-center rounded-t-md" src={src} alt="" />
@@ -22,7 +22,7 @@ function ProductCard({ src = defaultProduct, productPrice = "1,050,000", product
                         <Skeleton height={192} enableAnimation={true} />
                     )}
                 </div>
-                <div className="p-2">
+                <div className="p-4">
                     <div className="text-sm text-gray-700">{skeleton ? <>&#3647; {productPrice}</> : <Skeleton />}</div>
                     <div className="truncate">{skeleton ? productName : <Skeleton />}</div>
                     <div className="mt-1 text-sm text-gray-500">{skeleton ? "กรุงเทพมหานคร" : <Skeleton />}</div>
